fed901f80c659cd3671bff474c6a76a33bb7d203
Ali <89438079+AliBa1@users.noreply.github.com>
2025-05-09 10:28:49 -0700

    init sdl3

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,23 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!";
+int main(int argc, char *argv[]) {
+  if (SDL_Init(SDL_INIT_VIDEO) != true) {
+    std::cerr << "SDL_INIT Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_Window *win = SDL_CreateWindow("Hello SDL3", 640, 480, SDL_WINDOW_OPENGL);
+  if (!win) {
+    std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Delay(2000);
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+  std::cout << "SDL Initialized and Quit successfully!\n";
   return 0;
 }
-
