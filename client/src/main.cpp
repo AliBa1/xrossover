6392f1d9e8f7f0f3c4dddd08713d30ae35b6cf5e
Ali <89438079+AliBa1@users.noreply.github.com>
2025-05-10 20:52:00 -0700

    draw paddle, ball, and walls

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 const int screenWidth = 800;
 const int screenHeight = 600;
+const float thickness = 15.0;
+const float paddleHeight = 15.0 * 7;
+struct Vector2 {
+  float x;
+  float y;
+};
 
 class Game {
 public:
@@ -18,6 +24,9 @@ private:
   SDL_Window *window;
   SDL_Renderer *renderer;
   bool isRunning;
+
+  Vector2 ballPos;
+  Vector2 paddlePos;
 
   void ProcessInput();
   void UpdateGame();
@@ -41,7 +50,13 @@ bool Game::Initialize() {
     SDL_Log("SDL_CreateRenderer Error: %s", SDL_GetError());
     return false;
   }
-  
+
+  ballPos.x = screenWidth / 2.0;
+  ballPos.y = screenHeight / 2.0;
+
+  paddlePos.x = 100;
+  paddlePos.y = screenHeight / 2.0;
+
   isRunning = true;
   return true;
 }
@@ -74,6 +89,39 @@ void Game::ProcessInput() {
 void Game::GenerateOutput() {
   SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
   SDL_RenderClear(renderer);
+  
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  
+  // top wall
+  SDL_FRect wall{0.0, 0.0, static_cast<float>(screenWidth), thickness};
+  SDL_RenderFillRect(renderer, &wall);
+
+  // bottom wall
+  wall.y = screenHeight - thickness;
+  SDL_RenderFillRect(renderer, &wall);
+  
+  // right wall
+  wall.x = static_cast<float>(screenWidth) - thickness;
+  wall.y = 0;
+  wall.w = thickness;
+  wall.h = static_cast<float>(screenWidth);
+  SDL_RenderFillRect(renderer, &wall);
+  
+  SDL_FRect ball{
+    ballPos.x - thickness / 2,
+    ballPos.y - thickness / 2,
+    thickness,
+    thickness
+  };
+  SDL_RenderFillRect(renderer, &ball);
+  
+  SDL_FRect paddle{
+    paddlePos.x - thickness / 2,
+    paddlePos.y - (thickness / 2) - (paddleHeight/2),
+    thickness,
+    paddleHeight
+  };
+  SDL_RenderFillRect(renderer, &paddle);
   SDL_RenderPresent(renderer);
 }
 
