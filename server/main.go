656cbd219bb78c2f9a93b5f6e08188b29357a5bc
Ali <89438079+AliBa1@users.noreply.github.com>
2025-05-13 11:54:29 -0700

    udp connection

--- server/main.go
+++ server/main.go
@@ -7,29 +7,77 @@ import (
 )
 
 var (
-	PORT = "50000"
+	TCPPORT = "50000"
+	UDPPORT = "50001"
 )
 
 func main() {
 	fmt.Println("Welcome to the Game Server!")
-	// NOTE: switch to tcp6 if ipv6
-	listener, err := net.Listen("tcp", ":"+PORT)
+	go startTCP()
+	go startUDP()
+	select {}
+}
+
+func startTCP() {
+	listener, err := net.Listen("tcp", ":"+TCPPORT)
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
 	defer listener.Close()
 
-	fmt.Println("Listening on port", PORT)
+	fmt.Println("Listening TCP on port", TCPPORT)
 
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatalln("Error:", err)
 		}
-		go handleConnection(conn)
+		go handleTCPConnection(conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleTCPConnection(conn net.Conn) {
 	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			log.Fatalln("Error:", err)
+		}
+		log.Println("Recieved:", buffer[:n])
+	}
+}
+
+func startUDP() {
+	addr, err := net.ResolveUDPAddr("udp", ":"+UDPPORT)
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+
+	conn, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+	// defer conn.Close()
+
+	fmt.Println("Listening UDP on port", UDPPORT)
+
+	go handleUDPConnection(conn)
+}
+
+func handleUDPConnection(conn *net.UDPConn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 1024)
+
+	for {
+		n, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Fatalln("Error:", err)
+		}
+
+		log.Println("From:", addr, "Recieved:", buffer[:n])
+	}
 }
