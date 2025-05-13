7910337842dc69234f5cdfd38e7a9ba5cfee597d
Ali <89438079+AliBa1@users.noreply.github.com>
2025-05-13 11:17:36 -0700

    tcp listener

--- server/main.go
+++ server/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+var (
+	PORT = "50000"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Welcome to the Game Server!")
+	// NOTE: switch to tcp6 if ipv6
+	listener, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+	defer listener.Close()
+
+	fmt.Println("Listening on port", PORT)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("Error:", err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
 }
