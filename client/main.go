f3b9836f9a196c41770b0809ab649f50af1ca87d
Ali <89438079+AliBa1@users.noreply.github.com>
2025-05-14 15:42:28 -0700

    open window

--- client/main.go
+++ client/main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 func main() {
 	fmt.Println("Welcome to the client!")
+	rl.InitWindow(800, 450, "raylib [core] example - basic window")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		rl.EndDrawing()
+	}
 }
