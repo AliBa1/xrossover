ded90bbcc56b3556c801ddd7127cc9fbb9838338
Ali <89438079+AliBa1@users.noreply.github.com>
2025-05-15 16:47:40 -0700

    moving box

--- client/internal/game/game.go
+++ client/internal/game/game.go
@@ -1,6 +1,8 @@
 package game
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 const (
 	WIDTH  = 800
@@ -28,13 +30,14 @@ func (g *Game) initialize() {
 		Fovy:       45.0,
 		Projection: rl.CameraPerspective,
 	}
-	g.cubePosition = rl.Vector3{X: 0.0, Y: 0.0, Z: 0.0}
+	g.cubePosition = rl.Vector3{X: 0.0, Y: 1.0, Z: 0.0}
 
 	rl.SetTargetFPS(60)
 }
 
 func (g *Game) loop() {
 	for !rl.WindowShouldClose() {
+		g.processInput()
 		rl.BeginDrawing()
 		g.updateDrawing()
 		rl.EndDrawing()
@@ -49,12 +52,24 @@ func (g *Game) updateDrawing() {
 	g.update3DOutput()
 	rl.EndMode3D()
 
-	rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+	// rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
 	rl.DrawFPS(20, 20)
 }
 
 func (g *Game) update3DOutput() {
-	rl.DrawCube(g.cubePosition, 2.0, 2.0, 2.0, rl.Red)
-	rl.DrawCubeWires(g.cubePosition, 2.0, 2.0, 2.0, rl.Maroon)
+	rl.DrawCube(g.cubePosition, 1.0, 1.0, 1.0, rl.Red)
+	rl.DrawCubeWires(g.cubePosition, 1.0, 1.0, 1.0, rl.Maroon)
 	rl.DrawGrid(10, 1.0)
+}
+
+func (g *Game) processInput() {
+	if rl.IsKeyDown(rl.KeyW) {
+		g.cubePosition.Z -= 0.05
+	} else if rl.IsKeyDown(rl.KeyS) {
+		g.cubePosition.Z += 0.05
+	} else if rl.IsKeyDown(rl.KeyA) {
+		g.cubePosition.X -= 0.05
+	} else if rl.IsKeyDown(rl.KeyD) {
+		g.cubePosition.X += 0.05
+	}
 }
