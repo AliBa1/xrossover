d63c3f4a3edefa7e4ad521d57d4c42c50ac4405c
Ali <89438079+AliBa1@users.noreply.github.com>
2025-05-19 16:15:55 -0700

    clients array

--- server/main.go
+++ server/main.go
@@ -5,20 +5,25 @@ import (
 	"io"
 	"log"
 	"net"
+	"sync"
 )
 
 var (
-	TCPPORT = "50000"
-	UDPPORT = "50001"
+	TCPPort      = "50000"
+	UDPPort      = "50001"
+	clients      = map[string]*Client{}
+	clientsMutex sync.Mutex
+	// ClientID = "1"
 )
 
 type Client struct {
+	// ID      string
 	tcpConn net.Conn
 	udpConn net.UDPConn
+	// udpAddr *net.UDPAddr
 }
 
 func main() {
-	// var clients []Client
 	fmt.Println("Welcome to the Game Server!")
 	go startTCP()
 	go startUDP()
@@ -26,13 +31,13 @@ func main() {
 }
 
 func startTCP() {
-	listener, err := net.Listen("tcp", ":"+TCPPORT)
+	listener, err := net.Listen("tcp", ":"+TCPPort)
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
 	defer listener.Close()
 
-	fmt.Println("Listening TCP on port", TCPPORT)
+	fmt.Println("Listening TCP on port", TCPPort)
 
 	for {
 		conn, err := listener.Accept()
@@ -46,6 +51,16 @@ func startTCP() {
 func handleTCPConnection(conn net.Conn) {
 	defer conn.Close()
 
+	clientsMutex.Lock()
+	_, exists := clients[conn.RemoteAddr().String()]
+	if !exists {
+		clients[conn.RemoteAddr().String()] = &Client{tcpConn: conn}
+	} else {
+		clients[conn.RemoteAddr().String()].tcpConn = conn
+	}
+	// client := clients[conn.RemoteAddr().String()]
+	clientsMutex.Unlock()
+
 	buffer := make([]byte, 1024)
 
 	for {
@@ -53,9 +68,13 @@ func handleTCPConnection(conn net.Conn) {
 		if err != nil {
 			if err == io.EOF {
 				log.Println("Client Disconnected")
-				break
+			} else {
+				log.Println("Error:", err)
 			}
-			log.Println("Error:", err)
+
+			clientsMutex.Lock()
+			delete(clients, conn.RemoteAddr().String())
+			clientsMutex.Unlock()
 			break
 		}
 		log.Println("Recieved:", string(buffer[:n]))
@@ -63,7 +82,7 @@ func handleTCPConnection(conn net.Conn) {
 }
 
 func startUDP() {
-	addr, err := net.ResolveUDPAddr("udp", ":"+UDPPORT)
+	addr, err := net.ResolveUDPAddr("udp", ":"+UDPPort)
 	if err != nil {
 		log.Fatalln("Error:", err)
 	}
@@ -73,13 +92,23 @@ func startUDP() {
 		log.Fatalln("Error:", err)
 	}
 
-	fmt.Println("Listening UDP on port", UDPPORT)
+	fmt.Println("Listening UDP on port", UDPPort)
 
 	go handleUDPConnection(conn)
 }
 
 func handleUDPConnection(conn *net.UDPConn) {
 	defer conn.Close()
+
+	clientsMutex.Lock()
+	_, exists := clients[conn.RemoteAddr().String()]
+	if !exists {
+		clients[conn.RemoteAddr().String()] = &Client{udpConn: *conn}
+	} else {
+		clients[conn.RemoteAddr().String()].udpConn = *conn
+	}
+	// client := clients[conn.RemoteAddr().String()]
+	clientsMutex.Unlock()
 
 	buffer := make([]byte, 1024)
 
